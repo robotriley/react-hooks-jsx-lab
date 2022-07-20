c5704d98e179e7e229e96ed29ce05625a074dfbf
riley soloner <rileysoloner@gmail.com>
2022-07-19 20:14:02 -0400

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+    <h2>About Me</h2>
+    <p>Whats up guysssss hows it goinggggg thanks for reading.
+      This is Liz here. Yep I'm a dang web developer. I love my NYC tech life!
+      Rainbows, cats, PIZZAAAA!!!
+    </p>
+    <img src={image} alt="I made this" />
+  </div>;
 }
 
 export default About;
