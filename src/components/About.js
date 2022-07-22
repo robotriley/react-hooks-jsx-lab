7c3bdd229994cf493dd22c3f0f35490c7811d3dc
riley soloner <rileysoloner@gmail.com>
2022-07-22 15:58:32 -0400

    commit tree 16097044f566f245a9dfd9d5fc02659945eb40c6

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div id="about">About
+  return <div id="about">
     <h2>About Me</h2>
     <p>Whats up guysssss hows it goinggggg thanks for reading.
       This is Liz here. Yep I'm a dang web developer. I love my NYC tech life!
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Home from "./Home"
-import About from "./About"
 
 function NavBar() {
   // update the JSX being returned!
   return <nav>
-    <a href={"#home"}>Home</a>
-    <a href={"#about"} >About</a>
+    <a href="#home">Home</a>
+    <a href="#about" >About</a>
   </nav>;
 }
 
